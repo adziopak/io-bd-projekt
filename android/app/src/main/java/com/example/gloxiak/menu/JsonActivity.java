037cdfbcbd236b19be53b6c20878ed3604758958
damman96 <damianwrobel35@gmail.com>
2017-05-06 09:35:27 +0200

    Update JsonActivity.java
    json pobierający dane o budynkach

--- android/app/src/main/java/com/example/gloxiak/menu/JsonActivity.java
+++ android/app/src/main/java/com/example/gloxiak/menu/JsonActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,8 +27,8 @@ public class JsonActivity extends AppCompatActivity {
     private ProgressDialog pDialog;
     private ListView lv;
 
-    // URL to get contacts JSON
-    private static String url = "https://api.github.com/users/gloxiakx";
+    // URL : stąd pobieramy dane
+    private static String url = "http://skkshowcase.cba.pl/android/map?name=P";
 
     ArrayList<HashMap<String, String>> dataList;
 
@@ -63,54 +64,25 @@ public class JsonActivity extends AppCompatActivity {
         protected Void doInBackground(Void... arg0) {
             HttpHandler sh = new HttpHandler();
 
-            // Making a request to url and getting response
             String jsonStr = sh.makeServiceCall(url);
             Log.e(TAG, "Response from url: " + jsonStr);
 
             if (jsonStr != null) {
                 try {
-                    JSONObject c = new JSONObject(jsonStr);
 
-                    String login = c.getString("login");
-                    String id = c.getString("id");
-                    String avatar_url = c.getString("avatar_url");
-                    String gravatar_id = c.getString("gravatar_id");
-                    String url = c.getString("url");
-                    String html_url = c.getString("html_url");
-                    String followers_url = c.getString("followers_url");
-                    String following_url = c.getString("following_url");
-                    String gists_url = c.getString("gists_url");
-                    String starred_url = c.getString("starred_url");
-                    String subscriptions_url = c.getString("subscriptions_url");
-                    String organizations_url = c.getString("organizations_url");
-                    String repos_url = c.getString("repos_url");
-                    String events_url = c.getString("events_url");
-                    String received_events_url = c.getString("received_events_url");
-                    String type = c.getString("type");
-                    String site_admin = c.getString("site_admin");
-                    String name = c.getString("name");
-                    String company = c.getString("company");
-                    String blog = c.getString("blog");
-                    String location = c.getString("location");
-                    String email = c.getString("email");
-                    String hireable = c.getString("hireable");
-                    String bio = c.getString("bio");
-                    String public_repos = c.getString("public_repos");
-                    String public_gists = c.getString("public_gists");
-                    String followers = c.getString("followers");
-                    String following = c.getString("following");
-                    String created_at = c.getString("created_at");
-                    String updated_at = c.getString("updated_at");
+                    JSONArray dataArray = new JSONArray(jsonStr);
 
-                    HashMap<String, String> data = new HashMap<>();
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        JSONObject jsonObj = dataArray.getJSONObject(i);
 
-                    data.put("id", id);
-                    data.put("name", name);
-                    data.put("email", email);
-                    data.put("location", location);
+                        String name = jsonObj.getString("name");
 
-                    dataList.add(data);
+                        HashMap<String, String> data_tmp = new HashMap<>();
 
+                        data_tmp.put("name", name);
+
+                        dataList.add(data_tmp);
+                    }
                 } catch (final JSONException e) {
                     Log.e(TAG, "Json parsing error: " + e.getMessage());
                     runOnUiThread(new Runnable() {
@@ -152,9 +124,7 @@ public class JsonActivity extends AppCompatActivity {
              * */
             ListAdapter adapter = new SimpleAdapter(
                     JsonActivity.this, dataList,
-                    R.layout.list_item, new String[]{"name", "email",
-                    "location", "id"}, new int[]{R.id.name,
-                    R.id.email, R.id.location, R.id.id});
+                    R.layout.list_item, new String[]{"name"}, new int[]{R.id.name});
 
             lv.setAdapter(adapter);
         }
