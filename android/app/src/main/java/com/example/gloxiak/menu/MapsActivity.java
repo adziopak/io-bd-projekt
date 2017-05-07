04db29da1b487175cca9ed43997d1d9c16340d41
GloxiakX <gloxiak@gmail.com>
2017-05-07 14:32:43 +0200

    Pokazywanie lokalizacji android
    TODO:
    pobierać dane o polozeniu z bazy
    wyswietlac tylko 1 pin o polozeniu (teraz ich duzo bd)
    zrobic piny dla kazdego budynku (np literka V w pinie)

--- android/app/src/main/java/com/example/gloxiak/menu/MapsActivity.java
+++ android/app/src/main/java/com/example/gloxiak/menu/MapsActivity.java
@@ -1,28 +1,21 @@
 package com.example.gloxiak.menu;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Geocoder;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.identity.intents.Address;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,27 +25,26 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
+
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap mMap;
+    LocationManager locationManager;
+    String[] buildingNames = new String[]{"Budynek V", "Budynek K"};
+    String[] buildingSnipps = new String[]{"Biblioteka", "Chemia"};
+    Double[] buildingLat = new Double[]{50.0188232, 51.0188232};
+    Double[] buildingLng = new Double[]{21.9887356, 22.9887356};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (googleServicesAvailable()) { //zeby nie crashowalo
+        if (googleServicesAvailable()) {
             setContentView(R.layout.activity_maps);
             initMap();
-        } else {
-            //brak layoutu
+            showCurrentLocation();
         }
-    }
-
-    public void initMap() {
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
-        mapFragment.getMapAsync(this);
     }
 
     public boolean googleServicesAvailable() {
@@ -65,20 +57,94 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             dialog.show();
             return false;
         } else {
+            Toast.makeText(this, "Nie połączono z play services", Toast.LENGTH_LONG).show();
             return false;
         }
+    }
+
+    public void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    public void showCurrentLocation() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 10, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double lat = location.getLatitude();
+                    double lng = location.getLongitude();
+                    putMarker("test","test",lat,lng);
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        }
+        else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double lat = location.getLatitude();
+                    double lng = location.getLongitude();
+                    putMarker("test","test",lat,lng);
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+
+        }
+    }
+
+    private void goToLocation(double lat, double lng) {
+        LatLng latLng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, 14);
+        mMap.moveCamera(update);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter(){
+        goToLocation(buildingLat[0], buildingLng[0]);
+        putMarkers();
 
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter(){
             @Override
             public View getInfoWindow(Marker marker) {
                 return null;
             }
-
             @Override
             public View getInfoContents(Marker marker) {
                 View view = getLayoutInflater().inflate(R.layout.info_window, null);
@@ -87,34 +153,34 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return view;
             }
         });
-
-        MarkerOptions options = new MarkerOptions()
-                .title("Budynek V")
-                .snippet("test");
-        mMap.addMarker(options);
-
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             @Override
-            public boolean onMarkerClick(Marker marker) {
-                return false;
+            public void onInfoWindowClick(Marker marker) {
+                if (marker.getTitle().equals(buildingNames[0])) {
+                    Intent op1 = new Intent(MapsActivity.this, BuildingActivity.class);
+                    startActivity(op1);
+                }
             }
         });
+
+    }
+    private void putMarkers(){
+        putMarker(buildingNames[0], buildingSnipps[0], buildingLat[0], buildingLng[0]);
     }
 
-
-    private void goToLocation(double lat, double lng) {
-        LatLng latLng = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, 14);
-        mMap.moveCamera(update);
+    private void putMarker(String title, String snippet, double lat, double lng){
+        MarkerOptions options = new MarkerOptions()
+                .title(title)
+                .snippet(snippet)
+                .position(new LatLng(lat,lng));
+        mMap.addMarker(options);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -129,5 +195,4 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
