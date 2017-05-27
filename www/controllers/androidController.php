49c117c448fe6a6791ca4e40727215bbb862cdda
pologan <pgandorski@gmail.com>
2017-05-27 18:01:48 +0200

    Dodano wysyłanie pinów do androida

--- www/controllers/androidController.php
+++ www/controllers/androidController.php
@@ -53,7 +53,7 @@ class AndroidController
             $floor = $_GET['floor'];
             
             $dbconn = new DatabaseConnect;
-            $stmt = $dbconn->prepare("select m.id, b.name, m.floor, m.image, m.image_width, m.image_height, m.image_md5 from maps m left join buildings b on m.building_id = b.id where b.name=? and m.floor=?");
+            $stmt = $dbconn->prepare("select m.id, b.name, m.floor, m.image, m.image_width, m.image_height, m.image_md5, p.id, p.name, p.pos_x, p.pos_y from maps m left join buildings b on m.building_id = b.id left join pins p on p.map_id = m.id where b.name=? and m.floor=?");
             $stmt->bind_param("si",$name,$floor);
             $stmt->execute();
             
@@ -67,7 +67,7 @@ class AndroidController
             $name = $_GET['name'];
             
             $dbconn = new DatabaseConnect;
-            $stmt = $dbconn->prepare("select * from maps left join buildings on maps.building_id = buildings.id where buildings.name=?" );
+            $stmt = $dbconn->prepare("select * from maps left join buildings on maps.building_id = buildings.id left join pins on pins.map_id = maps.id where buildings.name=?" );
             $stmt->bind_param("s",$name);
             $stmt->execute();
             
